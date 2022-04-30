276caf8f6c11875fa595925e8960c7ded809656a
kimjiyeong <rlawldud335@naver.com>
2022-04-30 23:12:19 +0900

    4/30

---
+++ src/카카오인턴코테/레벨1/실패율.java
@@ -0,0 +1,60 @@
+package 카카오인턴코테.레벨1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 실패율 {
+
+    static class Solution {
+        public int[] solution(int N, int[] stages) {
+            int[] reached = new int[N+2];
+            int[] dontClear = new int[N+2];
+            for(int s : stages){
+                for(int i=1;i<=s;i++){
+                    reached[i]++;
+                }
+                dontClear[s]++;
+            }
+            ArrayList<Stage> stg = new ArrayList<>();
+            for(int i=1;i<=N;i++){
+                float fail = reached[i]==0?0:((float)dontClear[i]/(float)reached[i]);
+                stg.add(new Stage(i, fail));
+            }
+            Collections.sort(stg);
+            int[] answer = new int[N];
+            for (int i=0;i<N;i++){
+                answer[i] = stg.get(i).num;
+            }
+
+            return answer;
+        }
+
+        static class Stage implements Comparable<Stage>{
+            int num = 0;
+            float fail = 0;
+
+            public Stage(int num, float fail) {
+                this.num = num;
+                this.fail = fail;
+            }
+
+            @Override
+            public int compareTo(Stage o) {
+                if (fail>o.fail){
+                    return -1;
+                }else if(fail<o.fail){
+                    return 1;
+                }else{
+                    return num-o.num;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] ans = s.solution(4,new int[]{4,4,4,4,4});
+        System.out.println(Arrays.toString(ans));
+    }
+}
---
+++ src/카카오인턴코테/레벨3/합승택시요금.java
@@ -0,0 +1,49 @@
+package 카카오인턴코테.레벨3;
+
+public class 합승택시요금 {
+    static class Solution {
+        public int solution(int n, int s, int a, int b, int[][] fares) {
+            int[][] distance = new int[n+1][n+1];
+            int INF = 200000000;
+            for (int i=1;i<=n;i++){
+                for (int j=1;j<=n;j++){
+                    if (i==j){continue;}
+                    distance[i][j] = INF;
+                }
+            }
+            for (int[] f: fares){
+                distance[f[0]][f[1]] =f[2];
+                distance[f[1]][f[0]] =f[2];
+            }
+
+            for (int k = 1; k <= n; k++) {
+                for (int i = 1; i <= n; i++) {
+                    for (int j = 1; j <= n; j++) {
+                        distance[i][j] = Math.min(distance[i][j], (distance[i][k] + distance[k][j]));
+                    }
+                }
+            }
+
+            int min = distance[s][a]+distance[s][b];
+            for (int i=1;i<=n;i++){
+                int sum = distance[s][i];
+                sum += distance[i][a];
+                sum += distance[i][b];
+                if (sum<min){min = sum;}
+            }
+
+            return min;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] fares = {{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}};
+        int ans = s.solution(6,4,6,2,fares);
+        System.out.println(ans);
+//        String a = "[[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]";
+//        a = a.replaceAll("[\\[]","{");
+//        a = a.replaceAll("[\\]]","}");
+//        System.out.println(a);
+    }
+}
